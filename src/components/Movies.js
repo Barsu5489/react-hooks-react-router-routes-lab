4b3c460ccecf2a1fc4b45882a0aaf26fc8194622
barsu5489 <ebarsulai@gmail.com>
2023-01-22 11:48:03 +0300

    directors and movies accompished

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,7 +3,20 @@ import { movies } from "../data";
 
 function Movies() {
   return (<div><h1>Movies Page</h1>
-  
+  {movies.map((movie, index)=>{
+    return (
+      <div key={index}>
+        <h1>{movie.title}</h1>
+        <li>{movie.time}</li>
+        <ul>
+          {movie.genres.map((genre,index)=>{
+           return  <li key={index}>{genre}</li>
+          })}
+        </ul>
+
+      </div>
+    )
+  })}
   </div>);
 }
 
